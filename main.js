e1b6e1df56e79b3aea75499dd764020a6438f0b8
Salwa <salwasanaullahkhan@gmail.com>
2020-01-04 15:57:36 +0800

    i added new cards with vue

--- main.js
+++ main.js
@@ -28,8 +28,44 @@ var app2 = new Vue({
     chapters: [
       {
       no: 1,
-      link: nhnh
-    }
+      name: "Math Chapter 1 - Factors And Multiples",
+      link: "MathSec1Chap1.html",
+    },
+    {
+      no: 2,
+      name: "Math Chapter 2 - Real Numbers",
+      link: "MathSec1Chap2.html",
+    },
+    {
+      no: 3,
+      name: "Math Chapter 3 - Approximation And Estimation",
+      link: "MathSes1Chap3.html",
+    },
+    {
+      no: 4,
+      name: "Math Chapter 4 - Introduction to Algebra",
+      link: "MathSec1Chap4.html",
+    },
+    {
+      no: 5,
+      name: "i forgot",
+      link:"dfbh",
+    },
+    {
+      no: 6,
+      name: "i forgot",
+      link:"vjuvb",
+    },
+    {
+      no: 7,
+      name: "i forgot",
+      link:"vjuvb",
+    },
+    {
+      no: 8,
+      name: "i forgot",
+      link:"vjuvb",
+    },
     ]
   }
-})
+});
