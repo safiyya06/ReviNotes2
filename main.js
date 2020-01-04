1492db8f35d1d75325dd148c03a1baa24a2091ad
khansa123 <khansa_nauli_tamba@mai.sg>
2020-01-04 14:55:14 +0800

    add notes

--- main.js
+++ main.js
@@ -3,31 +3,21 @@
 var app = new Vue({
   el: '#app',
   data: {
-    notes: [],
-    addedNotes:"" 
-},
-created: function() {
-  if (localStorage.getItem("addnotes")){
-      this.notes = JSON.parse(localStorage.getItem("addnotes"));
-  }
-},
-  methods:{
-      addNotes: function (){
-          const addedNotes = {
-              name: this.newaddedNotes,
-              isDone: false
-          };
-          this.todos.unshift(newaddedNotes);
-          this.newaddedNotes = "";
-          this.persistData();
-},
-  deleteNotes: function(index) {
-      this.notes.splice(index, 1);
-      this.persistData();
-},
-  
-  persistData: function() {
-      localStorage.setItem("notes", JSON.stringify(this.notes));
+    notes: "",
   },
-},
+  created: function () {
+    if (localStorage.getItem("testNotes")) {
+      // this.notes = JSON.parse(localStorage.getItem("savedNotes"));
+      document.getElementById("exampleFormControlTextarea1").value = "hello"
+      this.notes = localStorage.getItem("testNotes")
+      }
+    },
+  methods: {
+    saveNotes: function () {
+      this.persistData();
+    },
+    persistData: function () {
+      localStorage.setItem("testNotes", this.notes)
+    },
+  },
 });
