3116674eb4eaa68d6190a232d96d9f9bee3ceb21
Salwa <salwasanaullahkhan@gmail.com>
2020-01-04 14:58:29 +0800

    added a second vue for another html page

--- main.js
+++ main.js
@@ -4,30 +4,43 @@ var app = new Vue({
   el: '#app',
   data: {
     notes: [],
-    addedNotes:"" 
-},
-created: function() {
-  if (localStorage.getItem("addnotes")){
+    addedNotes: ""
+  },
+  created: function () {
+    if (localStorage.getItem("addnotes")) {
       this.notes = JSON.parse(localStorage.getItem("addnotes"));
-  }
-},
-  methods:{
-      addNotes: function (){
-          const addedNotes = {
-              name: this.newaddedNotes,
-              isDone: false
-          };
-          this.todos.unshift(newaddedNotes);
-          this.newaddedNotes = "";
-          this.persistData();
-},
-  deleteNotes: function(index) {
+    }
+  },
+  methods: {
+    addNotes: function () {
+      const addedNotes = {
+        name: this.newaddedNotes,
+        isDone: false
+      };
+      this.todos.unshift(newaddedNotes);
+      this.newaddedNotes = "";
+      this.persistData();
+    },
+    deleteNotes: function (index) {
       this.notes.splice(index, 1);
       this.persistData();
-},
-  
-  persistData: function() {
+    },
+
+    persistData: function () {
       localStorage.setItem("notes", JSON.stringify(this.notes));
+    },
+
   },
-},
 });
+
+var app2 = new Vue({
+  el: '#app2',
+  data: {
+    chapters: [
+      {
+      no: 1,
+      link: nhnh
+    }
+    ]
+  }
+})
