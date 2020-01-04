a5887cb99fa6be7c82495ec188235c2e5646d701
khansa123 <khansa_nauli_tamba@mai.sg>
2020-01-04 10:35:00 +0800

    added pdf and tried to add notes

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
+
+
 var app = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
-    addedNotes: ""
-  },
-  methods: {
-    persistData: function() {
-      localStorage.setItem("", JSON.stringify());
-    }
+    todos: [],
+    addedNotes:"" 
+},
+created: function() {
+  if (localStorage.getItem("todos")){
+      this.todos = JSON.parse(localStorage.getItem("todos"));
   }
+},
+  methods:{
+      addNotes: function (){
+          const addedNotes = {
+              name: this.newaddedNotes,
+              isDone: false
+          };
+          this.todos.unshift(newTodo);
+          this.newTodoName = "";
+          this.persistData();
+},
+  deleteTodo: function(index) {
+      this.todos.splice(index, 1);
+      this.persistData();
+},
+  toggleDone: function(index) {
+      this.todos[index].isDone = !this.todos[index].isDone;
+      this.persistData();
+},
+  
+  persistData: function() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+  },
+},
 });
